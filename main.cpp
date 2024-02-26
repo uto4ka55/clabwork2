a6d9ce570d2fc3c4d8b9f65ed2cb02172cb6d360
Artem <artemkorzh@knu.ua>
2024-02-27 00:42:56 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,51 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 #include <string>
 
 using namespace std;
 
 int main() {
-    string xS;
-    int x;
-    int count = 0;
-    int triplets = 0;
+    string continueRunning;
+    do {
+        string nS, kS;
+        int n, k;
 
-    try {
-        cout << "Введіть x(x > 0): ";
-        cin >> xS;
+        try {
+            cout << "Введіть n1 (натуральне число): ";
+            cin >> kS;
+            cout << endl;
+            size_t pos;
+            k = stoi(kS, &pos);
+            if (pos < kS.size()|| k < 1) {throw 1;}
+        }
+        catch(...) {
+            cout << "Ви ввели некоректне число!";
+            continueRunning = "y";
+            cout << endl;
+            continue;
+        }
+        try {
+            cout << "Введіть n2(натуральне число яке >n1): ";
+            cin >> nS;
+            cout << endl;
+            size_t pos1;
+            n = stoi(nS, &pos1);
+            if (pos1 < nS.size()|| n < k) {throw 1;}
+        }
+        catch(...) {
+            cout << "Ви ввели некоректре число!";
+            cout << endl;
+            continueRunning = "y";
+            continue;
+        }
+
+        long double product = 1.0;
+        for (int i = k; i <= n; i++) {
+            double number =(1.0 - pow(-1.0, i - 1) / (3 * i - 1));
+            //cout << "Числове значення: " << number << endl;
+            product *= number;
+        }
+
+        cout << "Значення формули: " << product << endl;
+
+        cout << "Продовжити виконання програми? (y/n): ";
+        cin >> continueRunning;
         cout << endl;
-        size_t pos;
-        x = stoi(xS, &pos);
-        if (pos < xS.size()||x <= 0) {throw 1;}
-        }
-    catch(...) {
-        cout << "Ви ввели некоректре число!";
-        return 1;
-        }
+    } while (continueRunning == "y" || continueRunning == "Y");
 
-    vector<int> binary;
-    int num = x;
-    while(num) {
-        binary.push_back(num % 2);
-        num /= 2;
-    }
-    cout << "Цифра у двійковому коді: ";
-    for(int i = binary.size() - 1; i >= 0; i--) {
-        cout << binary[i];
-    }
-    cout << endl;
-
-    for (int i = 0; i <= 32; i++) {
-        if ((x & (1 << i)) != 0) {
-            count++;
-            if (count == 3) {
-                triplets++;
-                count = 0;
-            }
-        } else {
-            count = 0;
-        }
-    }
-    cout << "Кількість одиниць які зустрічаються 3 рази підряд: " << triplets << endl;
     return 0;
 }
